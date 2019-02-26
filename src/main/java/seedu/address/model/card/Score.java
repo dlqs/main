e172113eff0250254e68cfff20626e01954b60db
dlqs <donaldleeqs@gmail.com>
2019-02-26 15:25:27 +0800

    refactore score class again

--- src/main/java/seedu/address/model/card/Score.java
+++ src/main/java/seedu/address/model/card/Score.java
@@ -7,31 +7,71 @@ import javafx.util.Pair;
 
 /**
  * Represents a Card's score in the card.
- * Guarantees: immutable; is valid as declared in {@link #isValidScore(Pair<Integer, Integer>)}
+ * Guarantees: immutable; is valid as declared in {@link #isValidScore(Integer, Integer)}
  */
 public class Score {
 
     public static final String MESSAGE_CONSTRAINTS = "Score should be a pair of integers, each greater than or equal 0" +
             ", and it should not be blank";
 
-    public final Pair<Integer, Integer> value;
+    public final Integer correctAttempts;
+
+    public final Integer totalAttempts;
 
     /**
      * Constructs an {@code Score}.
      *
-     * @param score A valid score.
+     * @param correctAttempts A valid integer representing of attempts, >= 0
+     * @param totalAttempts A valid integer representing total attempts, >= correctAttempts
      */
-    public Score(Pair<Integer, Integer> score) {
+    public Score(Integer correctAttempts, Integer totalAttempts) {
+        requireNonNull(correctAttempts);
+        requireNonNull(totalAttempts);
+        checkArgument(isValidScore(correctAttempts, totalAttempts), MESSAGE_CONSTRAINTS);
+        this.correctAttempts = correctAttempts;
+        this.totalAttempts = totalAttempts;
+    }
+
+    /**
+     * Constructs an {@code Score}.
+     *
+     * @param score A valid score string, in form in a/b.
+     */
+    public Score(String score) {
         requireNonNull(score);
         checkArgument(isValidScore(score), MESSAGE_CONSTRAINTS);
-        value = score;
+        String[] splitNumbers = score.split("/");
+        this.correctAttempts = Integer.parseInt(splitNumbers[0]);
+        this.totalAttempts = Integer.parseInt(splitNumbers[1]);
     }
 
     /**
      * Returns true if a given score is valid score.
      */
-    public static boolean isValidScore(Pair<Integer, Integer> test) {
-        return test.getKey() >= 0 && test.getValue() >= test.getKey();
+    public static boolean isValidScore(Integer correctAttempts, Integer totalAttempts) {
+        return correctAttempts >= 0 && totalAttempts >= correctAttempts;
+    }
+
+    /**
+     * Returns true if a given string is formatted correctly, i.e. a/b where a and b are integers and 0 <= a <= b.
+     */
+    public static boolean isValidScore(String test) {
+        String[] splitNumbers = test.split("/");
+
+        if (splitNumbers.length != 2) {
+            return false;
+        }
+
+        Integer correctAttempts;
+        Integer totalAttempts;
+        try {
+            correctAttempts = Integer.parseInt(splitNumbers[0]);
+            totalAttempts = Integer.parseInt(splitNumbers[1]);
+        } catch (NumberFormatException ex) {
+            return false;
+        }
+
+        return isValidScore(correctAttempts, totalAttempts);
     }
 
     /**
@@ -40,28 +80,29 @@ public class Score {
      * @return score A score as double
      */
     public Double getAsDouble() {
-        if (value.getValue() == 0) {
+        if (totalAttempts == 0) {
             return 0.0;
         }
-        return (double)value.getKey()/value.getValue();
+        return (double)correctAttempts/totalAttempts;
     }
 
 
     @Override
     public String toString() {
-        return value.toString();
+        return String.format("%d/%d", correctAttempts, totalAttempts);
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof Score // instanceof handles nulls
-                && value.equals(((Score) other).value)); // state check
+                && correctAttempts.equals(((Score) other).correctAttempts)
+                && totalAttempts.equals(((Score) other).totalAttempts));
     }
 
     @Override
     public int hashCode() {
-        return value.hashCode();
+        return correctAttempts.hashCode()*totalAttempts.hashCode();
     }
 
 }
